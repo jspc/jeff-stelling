8b79505371e7162d2762dc341e9ad147021eee0f
jspc <james@zero-internet.org.uk>
2013-01-19 19:32:48 +0000

    Standardise, extend

--- lib/pundit/stelling.rb
+++ lib/pundit/stelling.rb
@@ -5,6 +5,10 @@
 class Stelling
   def self.say json
     parsed_json = JSON.parse( json )[0]
-    "Jeff Stelling: That #{ parsed_json['who'] } loves scoring against #{ parsed_json['against'] }!"
+    case rand(99) % 2
+    when 0
+      return "Jeff Stelling: That #{ parsed_json['who'] } loves scoring against #{ parsed_json['against'] }!"
+    when 1
+      return "Jeff Stelling: Goal between #{ parsed_json['against'] } and #{ parsed_json['for'] } and its #{ parsed_json['against'] } who got it"
   end
 end
