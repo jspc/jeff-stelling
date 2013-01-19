ced201fa57d075d41843984ddd200a42825c8deb
jspc <james@zero-internet.org.uk>
2013-01-19 19:33:41 +0000

    Open LeTiss; fix spelling

--- lib/pundit.rb
+++ lib/pundit.rb
@@ -3,18 +3,18 @@
 # Select a pundit and return a string based on that
 
 #require 'pundit/cammy.rb'
-#require 'pundit/mercer.rb'
+#require 'pundit/merson.rb'
 require 'pundit/thommo.rb'
-#require 'pundit/letis.rb'
+require 'pundit/letis.rb'
 require 'pundit/stelling.rb'
 
 class Pundit
   def initialize
     @pundit = [ 
 #               Cammy,
-#               Mercer,
+#               Merson,
                Thommo,
-#               LeTis,
+               LeTis,
                Stelling,
               ]
   end
