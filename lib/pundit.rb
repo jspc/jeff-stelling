f92eacb5bec6127190e38aa930addf21a93a6fc1
jspc <james@zero-internet.org.uk>
2013-01-19 17:28:55 +0000

    Start writing per-pundit

---
+++ lib/pundit.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#
+# Select a pundit and return a string based on that
+
+require 'pundit/cammy.rb'
+require 'pundit/mercer.rb'
+require 'pundit/thommo.rb'
+require 'pundit/letis.rb'
+require 'pundit/stelling.rb'
+
+class Pundit
+  def initialize
+    @pundit = [ 
+               Cammy,
+               Mercer,
+               Thommo,
+               LeTis,
+               Stelling,
+              ]
+  end
+
+  def get_pundit
+    @pundit.sample
+  end
+
+end
---
+++ lib/pundit/stelling.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#
+# Jeff Stelling
+
+class Stelling
+  def say who, team
+    "That #{who} loves scoring against #{team}!"
+  end
+end
