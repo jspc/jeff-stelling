072770ff82d132187b14ea321652387423890ba1
jspc <james@zero-internet.org.uk>
2013-01-20 19:47:36 +0000

    As per

--- lib/pundit/stelling.rb
+++ lib/pundit/stelling.rb
@@ -2,14 +2,15 @@
 #
 # Jeff Stelling
 
+
 class Stelling
+
   def self.say json
     parsed_json = JSON.parse( json )[0]
-    case rand(99) % 2
-    when 0
-      return "Jeff Stelling: That #{ parsed_json['who'] } loves scoring against #{ parsed_json['against'] }!"
-    when 1
-      return "Jeff Stelling: Goal between #{ parsed_json['against'] } and #{ parsed_json['for'] } and its #{ parsed_json['for'] } who got it"
-    end
+
+    [
+     "Jeff Stelling: That #{ parsed_json['who'] } loves scoring against #{ parsed_json['against'] }!",
+     "Jeff Stelling: Goal between #{ parsed_json['against'] } and #{ parsed_json['for'] } and its #{ parsed_json['for'] } who got it",
+    ].sample
   end
 end
