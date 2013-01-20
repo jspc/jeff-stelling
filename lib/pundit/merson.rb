0d7a9d5a31054be1f6cb049768f2ac186c196043
jspc <james@zero-internet.org.uk>
2013-01-20 19:46:43 +0000

    Tidy, redesign

--- lib/pundit/merson.rb
+++ lib/pundit/merson.rb
@@ -2,34 +2,34 @@
 #
 # Paul Merson object
 
+
 class Merson
+
   def self.say json
     parsed_json = JSON.parse( json )[0]
+
     if parsed_json['for'].downcase =~ /arsenal/
       return "Paul Merson: #{ arsenal( parsed_json ) }"
     elsif parsed_json['who'].downcase =~ /agbonlahor/  
       return "Paul Merson: What a play by Agbongsawhore!"
     else
-      case rand(99) % 3
-      when 0
-        return "Paul Merson: OOOAAAOOO! WHAT A GOAL! #{ mangle( parsed_json['who'].upcase ) }!"
-      when 1
-        return "Paul Merson: #{ parsed_json['who'] } is on fire! He must be on something, and I'd know!"
-      when 2
-        return "Paul Merson: #{ parsed_json['who'] } Has taken a gamble, and it's paid off, and I know gambling!"
-      end
+      [
+       "Paul Merson: OOOAAAOOO! WHAT A GOAL! #{ mangle( parsed_json['who'].upcase ) }!",
+       "Paul Merson: #{ parsed_json['who'] } is on fire! He must be on something, and I'd know!",
+       "Paul Merson: #{ parsed_json['who'] } Has taken a gamble, and it's paid off, and I know gambling!",
+      ].sample
     end
   end
 
   def self.arsenal json
-    return "OOOAAAOOO! WHAT A GOAL! #{ json['who'].upcase }!"
+    "OOOAAAOOO! WHAT A GOAL! #{ json['who'].upcase }!"
   end
 
   def self.mangle name
     name.gsub! "th", "sth"
     name.gsub! "k", "t"
     name.gsub! "g", ""
-    return name
+    name
   end
 
 end
