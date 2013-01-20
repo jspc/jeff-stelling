62d97792db6a81bba01157026857e123167413c5
jspc <james@zero-internet.org.uk>
2013-01-20 19:43:18 +0000

    Tidy Kammy (as it were)

--- lib/pundit/kammy.rb
+++ lib/pundit/kammy.rb
@@ -2,14 +2,15 @@
 #
 # Chris Kamara - http://en.wikipedia.org/wiki/Chris_Kamara
 
+
 class Kammy
+
   def self.say json
     parsed_json = JSON.parse( json )[0]
-    case rand(99) % 2
-    when 0
-      return "Kammy: UNBELIEVEABLE JEFF! #{ parsed_json['who'].upcase } HAS SCORED AGAINST #{ parsed_json['against'].upcase }!"
-    when 1
-      return "Kammy: I'M NOT SURE, BUT I THINK THERE'S BEEN A GOAL AGAINST #{ parsed_json['against'].upcase }!"
-    end  
+
+    [
+     "Kammy: UNBELIEVEABLE JEFF! #{ parsed_json['who'].upcase } HAS SCORED AGAINST #{ parsed_json['against'].upcase }!",
+     "Kammy: I'M NOT SURE, BUT I THINK THERE'S BEEN A GOAL AGAINST #{ parsed_json['against'].upcase }!",
+    ].sample
   end
 end
